8543a0aa1746200629bc311de71b4de4b5e034a9
Swetha <swetha@testpress.in>
2026-09-02 14:16:29 +0530

    Add image create view and form

--- images/views.py
+++ images/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
+from .models import Image
 
-# Create your views here.
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            new_item = form.save(commit=False)
+            new_item.user = request.user
+            new_item.save()
+            messages.success(request, 'Image added successfully')
+            return redirect(new_item.get_absolute_url())
+    else:
+        form = ImageCreateForm(data=request.GET)   
+    return render(request, 'images/image/create.html', {'section': 'images', 'form': form})
