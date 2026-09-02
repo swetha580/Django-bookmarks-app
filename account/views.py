89de67f49d5ee9bfa8f738215017726a3cfa19b4
Swetha <swetha@testpress.in>
2026-09-02 16:40:53 +0530

    Add activity stream to dashboard

--- account/views.py
+++ account/views.py
@@ -11,9 +11,20 @@ from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
 from actions.utils import create_action
 
+from actions.models import Action
+
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile') \
+                      .prefetch_related('target')[:10]
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard',
+                   'actions': actions})
 
 def register(request):
     if request.method == 'POST':
